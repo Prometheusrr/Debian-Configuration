fafe3937108c1b8149edcd336eadf3f0d5ba4b13
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-01-09 20:23:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 #Packages
-sudo apt install apt-transport-https curl git neofetch kitty sassc meson libglib2.0-dev build-essential dkms nautilus-nextcloud 
+sudo apt install apt-transport-https curl git neofetch kitty sassc meson libglib2.0-dev build-essential dkms nautilus-nextcloud variety
+sudo apt purge libreoffice* gnome-2048 gnome-chess gnome-color-manager gnome-contacts gnome-characters aisleriot gnome-games gnome-music gnome-logs gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles gnome-documents gnome-sudoku gnome-sushi gnome-taquin gnome-robots hitori firefox-esr five-or-more four-in-a-row lightsoff rhythmbox quadrapassel swell-foop gnome-sound-recorder gnome-screenshot cheese simple-scan gnome-font-viewer gnome-maps shotwell
+sudo apt autoremove
 
 #Move and Copy files
 rm -rf ~/.bashrc
-cp -r Etc/bash /$HOME/$USER/.bashrc
-cp -r /$HOME/$USER/Apps
-cp -r fonts/ /$HOME/$USER/.fonts
-rm /$HOME/$USER/.config/kitty
-cp -r kitty/ /$HOME/$USER/kitty
+cp -r Etc/bashrc $HOME/.bashrc
+cp -r Apps/ $HOME/Documents/Apps
+cp -r fonts/ $HOME/.fonts
+rm -rf $HOME/.config/kitty/
+cp -r kitty/ $HOME/.config/
 
 #Install Bash
-chmod +x Etc/Brave
-./Brave
+chmod +x Etc/brave
+./Etc/brave
 chmod +x Etc/pop-gtk
-./pop-gtk
+./Etc/pop-gtk
 chmod +x Etc/pop-icon
+./Etc/pop-icon
 
 #Anaconda install
 wget https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
@@ -24,14 +27,13 @@ chmod +x Anaconda*
 ./Anaconda*
 sudo cp -r anaconda.desktop /usr/share/applications/
 
-#Install Codium
-chmod +x Etc/vscodium
-./vscodium
-
 #Arduino
 wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz 
 tar -xf arduino*
-./arduino-1.8.19-linux64/install.sh
+sudo ./arduino-1.8.19/install.sh
+
+
+
 
 
 
