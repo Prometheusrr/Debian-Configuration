809f6ffa7e90b7958c2640957b4d6dca618fe372
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-10-31 01:40:58 -0400

    Update pipewire.sh

--- Etc/pipewire.sh
+++ Etc/pipewire.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#sudo apt policy pipewire pipewire-audio-client-libraries -y
-#sudo apt install pipewire pipewire-audio-client-libraries -y
+sudo apt policy pipewire pipewire-audio-client-libraries -y
+sudo apt install pipewire pipewire-audio-client-libraries -y
 
 
 #Create an empty file
-#sudo touch /etc/pipewire/media-session.d/with-pulseaudio
+sudo touch /etc/pipewire/media-session.d/with-pulseaudio
 
 #Create a pipewire-pulse service
-#sudo cp /usr/share/doc/pipewire/examples/systemd/user/pipewire-pulse.* /etc/systemd/user/
+sudo cp /usr/share/doc/pipewire/examples/systemd/user/pipewire-pulse.* /etc/systemd/user/
 
 #Reload the daemon file
-#sudo systemctl --user daemon-reload
+sudo systemctl --user daemon-reload
 
 #Disable PulseAudio service
-#systemctl --user --now disable pulseaudio.service pulseaudio.socket
+sudo systemctl --user --now disable pulseaudio.service pulseaudio.socket
 
 #Enable and start the PipeWire service:
-#systemctl --user --now enable pipewire pipewire-pulse
+sudo systemctl --user --now enable pipewire pipewire-pulse
 
 #Verify Changes
 pactl info | grep "Server Name" >> info.txt
@@ -25,5 +25,6 @@ cat info.txt
 rm info.txt
 
 #Reboot to set changes
-#sudo systemctl --user mask pulseaudio
+sudo systemctl --user mask pulseaudio
+
 
