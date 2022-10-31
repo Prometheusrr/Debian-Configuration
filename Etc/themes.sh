f1e31513bca9c1ce1dcee2a153d5f3a94a410aee
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-10-31 09:59:59 -0400

    Update themes.sh

--- Etc/themes.sh
+++ Etc/themes.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#Check Envirorment to apply theme
 if [ $DESKTOP_SESSION = 'plasma' ]; then
-    cp -r Theme/windark $HOME/.local/share/aurorae/themes/
+    mkdir $HOME/.local/share/aurorae 2>/dev/null
+    mkdir $HOME/.local/share/aurorae/themes 2>/dev/null
+    cp -r Theme/BreezeAurorae $HOME/.local/share/aurorae/themes/
+    ls $HOME/.local/share/aurorae/themes/
 elif [ $DESKTOP_SESSION = 'gnome' ]; then
     git clone https://github.com/vinceliuice/Orchis-theme.git
     ./Orchis-theme/install.sh -t default
