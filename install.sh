7c51c9adea1e57d18f701ddbe147c9ff7056ccad
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-10-31 02:27:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ echo "sudo apt update && sudo apt upgrade -y"
 
 #Switch to pipewire
 echo "SWITCHING TO PIPEWIRE"
-bash pipewire.sh
+bash Etc/pipewire.sh
 
 #Package to remove KDE or GNOME SETTING THEMES
 if [ $DESKTOP_SESSION = 'plasma' ]; then
-    echo "sudo apt purge kdeconnect kmail akregator gimp libreoffice* konqueror kaddressbook korganizer juk k3b kwalletmanager xterm kmouth -y"
-    echo "sudo apt install kitty gnome-disk-utility neofetch git curl linux-headers-amd64 kitty sassc meson libglib2.0-dev build-essential dkms flameshot -y"
+    sudo apt purge kdeconnect kmail akregator gimp libreoffice* konqueror kaddressbook korganizer juk k3b kwalletmanager xterm kmouth -y
+    sudo apt install kitty gnome-disk-utility neofetch git curl linux-headers-amd64 kitty sassc meson libglib2.0-dev build-essential dkms flameshot -y
 elif [ $DESKTOP_SESSION = 'gnome' ]; then
-    echo "sudo apt install apt-transport-https curl git neofetch kitty sassc meson libglib2.0-dev build-essential dkms flameshot -y"
-    echo "sudo apt purge libreoffice* gnome-2048 gnome-chess gnome-color-manager gnome-contacts gnome-characters aisleriot gnome-games gnome-music gnome-logs gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles gnome-documents gnome-sudoku gnome-sushi gnome-taquin gnome-robots hitori firefox-esr five-or-more four-in-a-row lightsoff rhythmbox quadrapassel swell-foop gnome-sound-recorder gnome-screenshot cheese simple-scan gnome-font-viewer gnome-maps shotwell -y"
+    sudo apt install apt-transport-https curl git neofetch kitty sassc meson libglib2.0-dev build-essential dkms flameshot -y
+    sudo apt purge libreoffice* gnome-2048 gnome-chess gnome-color-manager gnome-contacts gnome-characters aisleriot gnome-games gnome-music gnome-logs gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles gnome-documents gnome-sudoku gnome-sushi gnome-taquin gnome-robots hitori firefox-esr five-or-more four-in-a-row lightsoff rhythmbox quadrapassel swell-foop gnome-sound-recorder gnome-screenshot cheese simple-scan gnome-font-viewer gnome-maps shotwell -y
 fi
 
 #Setting Themes
-bash themes.sh
+bash Etc/themes.sh
 
 #Move and Copy files
 mv Photo-Videos/ $HOME/Pictures
