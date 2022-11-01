74599c91524d3ed8aa4673b1c281c9f0f9c16342
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-11-01 16:14:31 -0400

    Update anaconda.sh

--- Etc/anaconda.sh
+++ Etc/anaconda.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir $HOME/.local/share/applications
+
 sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
 
 wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
