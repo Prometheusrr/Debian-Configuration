94a331994e16d9a40696362fee18db5cb725e03e
Prometheusrr <34378849+Prometheusrr@users.noreply.github.com>
2022-11-01 17:42:03 -0400

    Update anaconda.sh

--- Etc/anaconda.sh
+++ Etc/anaconda.sh
@@ -2,11 +2,21 @@
 
 mkdir $HOME/.local/share/applications
 
-sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
+sudo apt-get install desktop-file-utils libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
 
 wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
 
 bash Anaconda3-2022.10-Linux-x86_64.sh
+
+echo "#!/usr/bin/env xdg-open" > anaconda.desktop
+echo '[Desktop Entry]' >> anaconda.desktop
+echo 'Name=Anaconda' >> anaconda.desktop
+echo 'Version=1.0' >> anaconda.desktop
+echo 'Type=Application' >> anaconda.desktop
+echo Exec=$HOME/anaconda3/bin/anaconda-navigator >> anaconda.desktop
+echo Icon=$HOME/anaconda3/lib/python3.7/site-packages/anaconda_navigator/static/images/anaconda-icon-256x256.png >> anaconda.desktop
+echo 'Comment=Open Anaconda Navigator' >> anaconda.desktop
+echo 'Terminal=false' >> anaconda.desktop
 
 cp anaconda.desktop ~/.local/share/applications/
 desktop-file-install --dir=$HOME/.local/share/applications anaconda.desktop
